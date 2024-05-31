253c1d184359d780b6afa4df160612031d6642e6
Gayani Nadeeshani <117030774+Gayani-Nadeeshani@users.noreply.github.com>
2024-05-31 13:03:37 +0530

    Add files via upload

---
+++ Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation = () => {
+
+    return(
+        <nav className="container">
+            <div className="logo">
+                <img src="./images/logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li href="#">MENU</li>
+                <li href="#">LOCATION</li>
+                <li href="#">ABOUT</li>
+                <li href="#">CONTACT</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+
+    );
+
+};
+
+export default Navigation;
